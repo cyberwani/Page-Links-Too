35587c392ea9775baac5d2d12a83e0ae064ba030
Gijs Jorissen <gijs@ginius.nl>
2012-10-24 18:54:33 +0200

    Get all posts

--- page_links_too.php
+++ page_links_too.php
@@ -248,7 +248,7 @@ class Page_Links_Too
 								foreach( $post_types as $post_type )
 								{
 									echo '<option value="0" disabled="disabled" style="font-weight: bold;">' . $post_type . '</option>';
-									$posts = get_posts( array( 'post_type' => $post_type, 'exclude' => $post->ID ) );
+									$posts = get_posts( array( 'post_type' => $post_type, 'exclude' => $post->ID, 'posts_per_page' => -1 ) );
 
 									foreach( $posts as $p )
 									{
